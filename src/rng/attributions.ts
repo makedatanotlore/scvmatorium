d506c443173dfc552830fffa8e5da76a653e8c3e
mcglincy <mattmcglincy@gmail.com>
2021-06-27 07:18:21 -0500

    Alphabetize attributions and authors for easier maintenance.

--- src/rng/attributions.ts
+++ src/rng/attributions.ts
@@ -1,35 +1,26 @@
 import { Attribution } from 'types/character';
 import {
-  makedatanotlore,
-  khordal,
-  ripleyc,
+  chalkdown,
   goatmansgoblet,
   greysonwhy,
-  chalkdown,
+  khordal,
+  makedatanotlore,
   michaelmars,
+  ripleyc,
 } from 'rng/authors';
 
-export const uncredited: Attribution = {
-  title: { id: 'content.shared.uncredited' },
-  id: 'uncredited',
-  authors: [],
-  url: '',
+export const anCailleach: Attribution = {
+  title: { id: 'content.michaelmars.anCailleach' },
+  id: 'anCailleach',
+  authors: [michaelmars],
+  url: 'https://www.drivethrurpg.com/product/349184/An-Cailleach-An-Irish-Folklore-Class-for-Mork-Borg',
 };
 
-export const scvmatorium: Attribution = {
-  title: { id: 'content.makedatanotlore.scvmatorium' },
-  id: 'scvmatorium',
-  authors: [makedatanotlore],
-  url:
-    'https://docs.google.com/document/d/1VPsCnoa4SDvoNTpUYR_oTwMz4kksyiV0iaEePjreHlc/edit?usp=sharing',
-};
-
-export const scvmfurther2: Attribution = {
-  title: { id: 'content.makedatanotlore.scvmfurther2' },
-  id: 'scvmfurther2',
-  authors: [makedatanotlore],
-  url:
-    'https://docs.google.com/document/d/1LOijYULDxCiABHlzf1K_Zl-Yig91CQA1t8GaoAFBA68/edit?usp=sharing',
+export const blightedMerman: Attribution = {
+  title: { id: 'content.chalkdown.blightedMerman' },
+  id: 'blightedMerman',
+  authors: [chalkdown],
+  url: 'https://chalkdown.itch.io/blighted-merman',
 };
 
 export const landlockedBuccaneer: Attribution = {
@@ -43,39 +34,7 @@ export const misbegottenRelict: Attribution = {
   title: { id: 'content.khordal.misbegottenRelict' },
   id: 'misbegottenRelict',
   authors: [khordal],
-  url:
-    'https://docs.google.com/document/d/1ndEAhYx7-g405r1saetGfSsG1QEOpIPhf_c4nS0lFB0',
-};
-
-export const sunScorchedZealot: Attribution = {
-  title: { id: 'content.khordal.sunScorchedZealot' },
-  id: 'sunScorchedZealot',
-  authors: [khordal],
-  url:
-    'https://docs.google.com/document/d/1hOm-kmOIpLW9-4SUGWUriJ6hTO32Fwi5LmDDfJCOukE/edit',
-};
-
-export const rottingRecalcitrant: Attribution = {
-  title: { id: 'content.ripleyc.rottingRecalcitrant' },
-  id: 'rottingRecalcitrant',
-  authors: [ripleyc],
-  url: 'https://makedatanotlore.itch.io/rotting-recalcitrant',
-};
-
-export const suitorKnightOfKergus: Attribution = {
-  title: { id: 'content.goatmansgoblet.suitorKnightOfKergus' },
-  id: 'suitorKnightOfKergus',
-  authors: [goatmansgoblet],
-  url:
-    'https://docs.google.com/document/d/1R-VCZBrmDupJhh39IpdutQHZ4DavY0rrE94DLu2tDig/edit',
-};
-
-export const theWretchedUsurper: Attribution = {
-  title: { id: 'content.goatmansgoblet.theWretchedUsurper' },
-  id: 'theWretchedUsurper',
-  authors: [goatmansgoblet],
-  url:
-    'https://docs.google.com/document/d/1PrGI1hOmimcDwQotfdCAjjhWvtIfnXaldFfs0YFivDg/edit',
+  url: 'https://docs.google.com/document/d/1ndEAhYx7-g405r1saetGfSsG1QEOpIPhf_c4nS0lFB0',
 };
 
 export const psyberDevangelist: Attribution = {
@@ -85,18 +44,53 @@ export const psyberDevangelist: Attribution = {
   url: 'https://greysonwhy.itch.io/the-psyber-devangelist-and-other-psi-sci-fi',
 };
 
-export const blightedMerman: Attribution = {
-  title: { id: 'content.chalkdown.blightedMerman' },
-  id: 'blightedMerman',
-  authors: [chalkdown],
-  url: 'https://chalkdown.itch.io/blighted-merman',
+export const rottingRecalcitrant: Attribution = {
+  title: { id: 'content.ripleyc.rottingRecalcitrant' },
+  id: 'rottingRecalcitrant',
+  authors: [ripleyc],
+  url: 'https://makedatanotlore.itch.io/rotting-recalcitrant',
 };
 
-export const anCailleach: Attribution = {
-  title: { id: 'content.michaelmars.anCailleach' },
-  id: 'anCailleach',
-  authors: [michaelmars],
-  url: 'https://www.drivethrurpg.com/product/349184/An-Cailleach-An-Irish-Folklore-Class-for-Mork-Borg',
+export const scvmatorium: Attribution = {
+  title: { id: 'content.makedatanotlore.scvmatorium' },
+  id: 'scvmatorium',
+  authors: [makedatanotlore],
+  url: 'https://docs.google.com/document/d/1VPsCnoa4SDvoNTpUYR_oTwMz4kksyiV0iaEePjreHlc/edit?usp=sharing',
+};
+
+export const scvmfurther2: Attribution = {
+  title: { id: 'content.makedatanotlore.scvmfurther2' },
+  id: 'scvmfurther2',
+  authors: [makedatanotlore],
+  url: 'https://docs.google.com/document/d/1LOijYULDxCiABHlzf1K_Zl-Yig91CQA1t8GaoAFBA68/edit?usp=sharing',
+};
+
+export const suitorKnightOfKergus: Attribution = {
+  title: { id: 'content.goatmansgoblet.suitorKnightOfKergus' },
+  id: 'suitorKnightOfKergus',
+  authors: [goatmansgoblet],
+  url: 'https://docs.google.com/document/d/1R-VCZBrmDupJhh39IpdutQHZ4DavY0rrE94DLu2tDig/edit',
+};
+
+export const sunScorchedZealot: Attribution = {
+  title: { id: 'content.khordal.sunScorchedZealot' },
+  id: 'sunScorchedZealot',
+  authors: [khordal],
+  url: 'https://docs.google.com/document/d/1hOm-kmOIpLW9-4SUGWUriJ6hTO32Fwi5LmDDfJCOukE/edit',
+};
+
+export const theWretchedUsurper: Attribution = {
+  title: { id: 'content.goatmansgoblet.theWretchedUsurper' },
+  id: 'theWretchedUsurper',
+  authors: [goatmansgoblet],
+  url: 'https://docs.google.com/document/d/1PrGI1hOmimcDwQotfdCAjjhWvtIfnXaldFfs0YFivDg/edit',
+};
+
+export const uncredited: Attribution = {
+  title: { id: 'content.shared.uncredited' },
+  id: 'uncredited',
+  authors: [],
+  url: '',
 };
 
 export const contentAttributions = [scvmatorium, scvmfurther2];
--- src/rng/authors.ts
+++ src/rng/authors.ts
@@ -1,18 +1,8 @@
 import { Author } from 'types/character';
 
-export const makedatanotlore: Author = {
-  name: 'Karl Druid',
-  url: 'https://makedatanotlore.itch.io/',
-};
-
-export const khordal: Author = {
-  name: 'Nick T',
-  url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-};
-
-export const ripleyc: Author = {
-  name: 'Ripley C',
-  url: 'https://ripleyc.itch.io/',
+export const chalkdown: Author = {
+  name: 'Chalkdown & Djuun',
+  url: 'https://chalkdown.itch.io/',
 };
 
 export const goatmansgoblet: Author = {
@@ -25,12 +15,22 @@ export const greysonwhy: Author = {
   url: 'https://greysonwhy.itch.io/',
 };
 
-export const chalkdown: Author = {
-  name: 'Chalkdown & Djuun',
-  url: 'https://chalkdown.itch.io/',
+export const khordal: Author = {
+  name: 'Nick T',
+  url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+};
+
+export const makedatanotlore: Author = {
+  name: 'Karl Druid',
+  url: 'https://makedatanotlore.itch.io/',
 };
 
 export const michaelmars: Author = {
   name: 'Michael Mars',
   url: 'https://www.drivethrurpg.com/browse/pub/18364/Michael-Mars',
+};
+
+export const ripleyc: Author = {
+  name: 'Ripley C',
+  url: 'https://ripleyc.itch.io/',
 };
