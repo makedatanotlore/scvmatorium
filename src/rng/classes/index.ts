a102137209827cd6f8265cb62aa0c512106d41fc
mcglincy <mattmcglincy@gmail.com>
2021-07-03 21:03:09 -0500

    Add Corpse Plunderer class by Harkenstone.

--- src/rng/classes/index.ts
+++ src/rng/classes/index.ts
@@ -3,6 +3,7 @@ import { suitorKnightOfKergus } from 'rng/goatmansgoblet/suitorKnightOfKergus';
 import { theWretchedUsurper } from 'rng/goatmansgoblet/theWretchedUsurper';
 import { cannibalCook } from 'rng/graytide/cannibalCook';
 import { psyberDevangelist } from 'rng/greysonwhy/psyberDevangelist';
+import { corpsePlunderer } from 'rng/harkenstone/corpsePlunderer';
 import { mutatedGoblin } from 'rng/heckinviv/mutatedGoblin';
 import { misbegottenRelict } from 'rng/khordal/misbegottenRelict';
 import { sunScorchedZealot } from 'rng/khordal/sunScorchedZealot';
@@ -14,18 +15,19 @@ import { betrayedPhantom } from 'rng/sashadeath/betrayedPhantom';
 import { nachthex } from 'rng/stregaflora/nachthex';
 
 export const classes = {
-  anCailleach,
-  betrayedPhantom,
-  blightedMerman,
-  cannibalCook,
-  fugitiveKnight,
-  landlockedBuccaneer,
-  misbegottenRelict,
-  mutatedGoblin,
-  nachthex,
-  psyberDevangelist,
-  rottingRecalcitrant,
-  sunScorchedZealot,
-  suitorKnightOfKergus,
-  theWretchedUsurper,
+  // anCailleach,
+  // betrayedPhantom,
+  // blightedMerman,
+  // cannibalCook,
+  corpsePlunderer,
+  // fugitiveKnight,
+  // landlockedBuccaneer,
+  // misbegottenRelict,
+  // mutatedGoblin,
+  // nachthex,
+  // psyberDevangelist,
+  // rottingRecalcitrant,
+  // sunScorchedZealot,
+  // suitorKnightOfKergus,
+  // theWretchedUsurper,
 };
