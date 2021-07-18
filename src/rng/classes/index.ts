79e44ae8bb344d5e44d52078802215659da4cc02
makedatanotlore <kddruid@gmail.com>
2021-07-18 12:06:21 +0200

    Revert "Add script to autogenerate src/rng/classes/index.ts and src/translations/en_US/content/index.ts. Fix bug in unheroicFeats/messages.json."
    This reverts commit 2bec9b56

---
+++ src/rng/classes/index.ts
@@ -0,0 +1,41 @@
+import { deathWitch } from 'rng/anadversary/deathWitch';
+import { brazenBlacksmith } from 'rng/ceph/brazenBlacksmith';
+import { blightedMerman } from 'rng/chalkdown/blightedMerman';
+import { suitorKnightOfKergus } from 'rng/goatmansgoblet/suitorKnightOfKergus';
+import { theWretchedUsurper } from 'rng/goatmansgoblet/theWretchedUsurper';
+import { cannibalCook } from 'rng/graytide/cannibalCook';
+import { psyberDevangelist } from 'rng/greysonwhy/psyberDevangelist';
+import { corpsePlunderer } from 'rng/harkenstone/corpsePlunderer';
+import { mutatedGoblin } from 'rng/heckinviv/mutatedGoblin';
+import { misbegottenRelict } from 'rng/khordal/misbegottenRelict';
+import { sunScorchedZealot } from 'rng/khordal/sunScorchedZealot';
+import { landlockedBuccaneer } from 'rng/makedatanotlore/landlockedBuccaneer';
+import { anCailleach } from 'rng/michaelmars/anCailleach'
+import { sinfulSailor } from 'rng/michaelmars/sinfulSailor'
+import { fugitiveKnight } from 'rng/neonon/fugitiveKnight';
+import { tormentedToymaker } from 'rng/reverendunclebastard/tormentedToymaker';
+import { rottingRecalcitrant } from 'rng/ripleyc/rottingRecalcitrant';
+import { betrayedPhantom } from 'rng/sashadeath/betrayedPhantom';
+import { nachthex } from 'rng/stregaflora/nachthex';
+
+export const classes = {
+  anCailleach,
+  betrayedPhantom,
+  blightedMerman,
+  brazenBlacksmith,
+  cannibalCook,
+  corpsePlunderer,
+  deathWitch,
+  fugitiveKnight,
+  landlockedBuccaneer,
+  misbegottenRelict,
+  mutatedGoblin,
+  nachthex,
+  psyberDevangelist,
+  rottingRecalcitrant,
+  sinfulSailor,
+  sunScorchedZealot,
+  suitorKnightOfKergus,
+  theWretchedUsurper,
+  tormentedToymaker,
+};
